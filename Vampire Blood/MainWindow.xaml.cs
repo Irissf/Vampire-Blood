46a5c52529f3ad6e6b76364d987e0ede549d22f5
Irissf <iris.seijo@gmail.com>
2022-06-14 11:45:39 +0200

    Agregar archivos de proyecto.

---
+++ Vampire Blood/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Vampire_Blood
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Window_Actions();
+        }
+
+        /// <summary>
+        /// Para mover la pantalla al hacer click sobre ella
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+                this.DragMove();
+        }
+
+        private void Window_Actions()
+        {
+            btnMinimeze.Click += (sender, e) => { WindowState = WindowState.Minimized; };
+            btnMaximize.Click += (sender, e) => { WindowState = WindowState == WindowState.Maximized ? WindowState.Normal : WindowState.Maximized; };
+            btnClose.Click += (sender, e) => { Close(); };
+        }
+    }
+}
